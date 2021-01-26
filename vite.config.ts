abd402a7303973d2c5f6937f15f88dc58c2c7d0d
bqy_fe <1743369777@qq.com>
2021-01-26 22:59:50 +0800

    chore: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,78 @@
-import {defineConfig} from 'vite'
+import type { UserConfig, ConfigEnv } from 'vite'
+import { loadEnv } from 'vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { resolve } from 'path'
 
-const pathSrc = path.resolve(__dirname, "./src")
+const CWD = process.cwd()
 
-export default defineConfig({
-    alias: {
-        '@': pathSrc,
-    },
-    esbuild: {
-      target: 'esnext'
-    },
-    css: {
-        preprocessorOptions: {
-            less: {
-                modifyVars: {},
-                javascriptEnabled: true,
-            },
-            scss: {
-                // https://github.com/vitejs/vite/issues/520
-                additionalData: `@import "src/styles/global.scss";`,
+// 环境变量
+const BASE_ENV_CONFIG = loadEnv('', CWD)
+const DEV_ENV_CONFIG = loadEnv('development', CWD)
+const PROD_ENV_CONFIG = loadEnv('production', CWD)
+
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+    // 环境变量
+    const {VITE_BASE_URL,VITE_DROP_CONSOLE} = loadEnv(mode, CWD)
+
+    // const isBuild = command === 'build';
+
+    return {
+        base: VITE_BASE_URL,
+        esbuild: {
+            // target: 'es2015'
+        },
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
+        plugins: [
+            vue(),
+            vueJsx({
+                // options are passed on to @vue/babel-plugin-jsx
+            }),
+        ],
+        css: {
+            preprocessorOptions: {
+                less: {
+                    modifyVars: {},
+                    javascriptEnabled: true
+                },
+                scss: {
+                    additionalData: `@import "src/styles/global.scss";`,
+                }
             }
         },
-    },
-    plugins: [
-        vue(),
-        vueJsx({
-            // options are passed on to @vue/babel-plugin-jsx
-        })
-    ],
-    optimizeDeps: {
-        include: [
-            'moment',
-            '@ant-design/icons-vue',
-            'lodash',
-            'ant-design-vue/es/locale/zh_CN',
-            'moment/locale/zh-cn',
-            'ant-design-vue/es/locale/en_US',
-        ],
-    },
-    server: {
-        proxy: {
-            '/api': {
-                // target: 'http://us-la-cn2.sakurafrp.com:59660',
-                target: 'http://localhost:7001',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '/api/v1')
+        server: {
+            port: 8888,
+            proxy: {
+                '/api': {
+                    target: 'http://jp-tyo-dvm.sakurafrp.com:63922',
+                    // target: 'http://localhost:7001',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '/api/v1')
+                }
             }
+        },
+        optimizeDeps: {
+            include: [
+                'moment',
+                '@ant-design/icons-vue',
+                'lodash',
+                'ant-design-vue/es/locale/zh_CN',
+                'moment/locale/zh-cn',
+                'ant-design-vue/es/locale/en_US',
+            ],
+            exclude: [
+                // 'ant-design-vue'
+            ]
+        },
+        build: {
+            terserOptions: {
+                compress: {
+                    keep_infinity: true,
+                    drop_console: VITE_DROP_CONSOLE,
+                },
+            },
         }
     }
-})
+}
