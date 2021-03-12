ff5f7658b453970eef92050c2887f20de0024eb0
xuezhangbo <18811464010@163.com>
2021-03-12 10:21:01 +0800

    style:更改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,11 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    "vue/no-unused-components": "off",
-    'vue/no-unused-vars': 'off',
-    'vue/no-v-for-template-key-on-child': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/camelcase': 'off',
+    "vue/no-unused-components": 1,
+    'vue/no-unused-vars': 1,
+    'vue/no-v-for-template-key-on-child': 1,
+    '@typescript-eslint/no-var-requires': 1,
+    '@typescript-eslint/camelcase': 1,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
